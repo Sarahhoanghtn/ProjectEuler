12957a48cbce4c4f63ad5c7f3b0f8cc5c6267af1
Sarah Hoang <134144574+Sarahhoanghtn@users.noreply.github.com>
2024-12-25 15:58:20 -0500

    Create PE004.java
    Adding solution to problem 4

---
+++ 4/PE004.java
@@ -0,0 +1,30 @@
+import java.lang.StringBuilder;
+
+public class PE004 {
+    public static int product = 0;
+
+    public static void main(String[] args) {
+        System.out.println(largestProduct(101, 999));
+    }
+  
+    private static boolean isPal() {
+        String pal = String.valueOf(product);
+        StringBuilder pal2 = new StringBuilder(""+ product);
+        pal2.reverse();
+        return pal.equals(pal2.toString());
+    }
+
+    private static int largestProduct (int smallest, int biggest) {
+        int largestProduct = 1;
+
+        for (int i = biggest; i > smallest; i--) {
+            for (int j = biggest; j > smallest; j--) {
+                product = i * j;
+                if (isPal() && product > largestProduct) {
+                    largestProduct = product;
+                }
+            }
+        }
+        return largestProduct;
+    }
+}
