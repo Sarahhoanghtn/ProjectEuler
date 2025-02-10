c31ecce5d847b1105af84f90ac27b632f78cc6d6
Sarah Hoang <134144574+Sarahhoanghtn@users.noreply.github.com>
2025-02-09 20:09:04 -0500

    Re-formatting

--- 1-50/1-9/2/PE002.java
+++ 1-50/1-9/2/PE002.java
@@ -1,16 +1,19 @@
 public class PE002 {
     public static void main(String[] args) {
-        long sum = 2;
-        long ef1 = 0;
-        long ef2 = 2;
         long limit = 4000000;
-        while (ef2 <= limit) {
-            long ef3 = 4 * ef2 + ef1;
-            if (ef3 > limit) break;
-            ef1 = ef2;
-            ef2 = ef3;
-            sum += ef2;
+        long sum = 2;
+        long evenFib1 = 0;
+        long evenFib2 = 2;
+
+        while (true) {
+            long evenFib3 = 4 * evenFib2 + evenFib1;
+            if (evenFib3 > limit) break;
+
+            sum += evenFib3;
+            evenFib1 = evenFib2;
+            evenFib2 = evenFib3;
         }
+
         System.out.println(sum);
     }
 }
