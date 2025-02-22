51fcd86e2b4aa97b7a30b44709ee73e02c72a1e2
Sarah Hoang <134144574+Sarahhoanghtn@users.noreply.github.com>
2025-02-22 14:51:59 -0500

    Updating solutions to problem 5

--- 1-50/1-9/5/PE005.java
+++ 1-50/1-9/5/PE005.java
@@ -1,15 +1,15 @@
 public class PE005 {
     public static void main (String[] args) {
-        System.out.println(evenlyDividedBy1To20());
+        System.out.println(PE005());
     }
     
-    static int evenlyDividedBy1To20() {
+    static double PE005() {
         int prod = 20 * 19;
-        int i = Math.ceil(2520 / prod) * prod; // find the lowest number divisible by 19 * 20 > 2520
+        double i = Math.ceil(2520 / prod) * prod; // find the lowest number divisible by 19 * 20 > 2520
         
         while (true) {
             i += prod; // i is divisible by 19 and 20 only if i is divisible by 19 * 20
-            for (int j = 20; j > 10; j--) { // if divisible by 11-20, i is divisible by 1-10
+            for (int j = 18; j > 10; j--) { // if divisible by 11-20, i is divisible by 1-10
                 if (i % j != 0) break;
                 if (j == 11) return i;
             }
