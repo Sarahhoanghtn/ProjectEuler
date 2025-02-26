eae790cb7cdb6bb6e0e4852dc31d62bcb5ab1cf2
Sarah Hoang <134144574+Sarahhoanghtn@users.noreply.github.com>
2025-02-26 09:26:42 -0500

    Update PE006.java
    Updating the solution to problem 6

--- 1-50/1-9/6/PE006.java
+++ 1-50/1-9/6/PE006.java
@@ -1,21 +1,19 @@
 public class PE006 {
-    public static void main(String[] args) {
-        System.out.println(squareSum() - sumSquare());
-    }
-
-    private static int sumSquare() {
+    private static double sumSquare() {
         double sum = 0;
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i < 101; i++) {
             sum += i * i;
         }
-        return (int) sum;
+
+        return sum;
     }
 
-    private static int squareSum() {
-        int sum = 0;
-        for (int i = 1; i <= 100; i++) {
-            sum += i;
-        }
-        return sum * sum;
+    private static double squareSum() {
+        double sum = (100.0 / 2) * (1 + 100); // Arithmetic Sequence
+        return Math.pow(sum, 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println((int) (squareSum() - sumSquare()));
     }
 }
